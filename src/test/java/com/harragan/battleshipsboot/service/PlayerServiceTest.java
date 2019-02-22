722c720f585a85c66b1b8730d61f165e87440009
95Kevid <kevinharragan@googlemail.com>
2019-02-22 22:12:37 +0000

    Attempt at multistage docker file ammendment to travis file to enable remote image building

--- src/test/java/com/harragan/battleshipsboot/service/PlayerServiceTest.java
+++ src/test/java/com/harragan/battleshipsboot/service/PlayerServiceTest.java
@@ -48,7 +48,7 @@ public class PlayerServiceTest {
     public void givenAPlayeridAPlayerIsMarkedAsStarted() {
         Player playerJo = new Player("Jo White");
         playerJo.setId(1);
-        
+
         playerJo.setGameArena(new GameArena());
         playerJo.getGameArena().setAllShipsPlaced(true);
         when(playerRepository.findById(playerJo.getId())).thenReturn(Optional.of(playerJo));
