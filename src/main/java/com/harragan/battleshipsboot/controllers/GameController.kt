aec145b7d5fa76c957d64f21e5037c339e93266d
95Kevid <kevinharragan@googlemail.com>
2019-03-08 13:28:15 +0000

    GameController return type changed to int

--- src/main/java/com/harragan/battleshipsboot/controllers/GameController.kt
+++ src/main/java/com/harragan/battleshipsboot/controllers/GameController.kt
@@ -4,17 +4,14 @@ import com.harragan.battleshipsboot.service.GameService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class GameController(private val gameService: GameService) {
 
+    @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(value = ["/creategame/{numberOfPlayers}/{arenaSize}"], method = [RequestMethod.POST])
-    fun createGame(@PathVariable numberOfPlayers: Int, @PathVariable arenaSize: Int): ResponseEntity<Int> {
-        val gameId = gameService!!.createGame(numberOfPlayers, arenaSize)
-        return ResponseEntity(gameId, HttpStatus.CREATED)
+    fun createGame(@PathVariable numberOfPlayers: Int, @PathVariable arenaSize: Int): Int {
+        return gameService!!.createGame(numberOfPlayers, arenaSize)
     }
 }
